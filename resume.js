24b97c18bac94bc50704f60ecee39b074020ee52
Roman Ožana <roman@ozana.cz>
2021-03-02 10:18:12 +0100

    CV to resume

--- resume.js
+++ resume.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	await page.goto('https://ozana.cz/resume', {waitUntil: 'networkidle2'});
+	await page.goto('http://localhost:3000/resume', {waitUntil: 'networkidle2'});
 	await page.pdf({
 		path: 'public/cv.pdf',
 		format: 'letter'
