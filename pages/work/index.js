cae8d0bb40e77ec638005812bb8da551a2e8aa8c
Roman Ožana <roman@ozana.cz>
2023-12-08 10:25:27 +0100

    Add CMD-dialog

--- pages/work/index.js
+++ pages/work/index.js
@@ -20,6 +20,13 @@ const ShareIcon = () =>
     <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z"/>
   </svg>;
 
+const GitHubIcon = () =>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-5 h-5 mr-2" viewBox="0 0 16 16">
+    <path
+      d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+  </svg>;
+
+
 const Testomato = () =>
   <article className="bg-[#b72b2a] grid lg:grid-cols-2 md:p-12 md:gap-12 p-6 lg:p-32 lg:gap-32 text-white">
     <div>
@@ -129,6 +136,28 @@ const Sphido = () =>
     </p>
   </article>;
 
+const CmdDialog = () =>
+  <article className="text-center bg-gray-800 p-6 md:p-24 lg:p-32 text-blue-50">
+    <h2 className="text-5xl lg:text-6xl xl:text-8xl font-mono font-semibold mb-4">
+      <a href="https://github.com/OzzyCzech/cmd-dialog/" target="_blank" className="text-inherit hover:text-blue-100 hover:no-underline">cmd-dialog</a>
+    </h2>
+    <p className="my-2 text-2xl">
+      Command dialog and keyboard shortcuts palette for web apps
+    </p>
+    <p className="flex flex-wrap items-center justify-center gap-2 my-6">
+      <a href="https://www.npmjs.com/package/cmd-dialog" target="_blank"><img src="https://img.shields.io/npm/dm/cmd-dialog?style=for-the-badge" alt="Downloads"/></a>
+      <a href="https://www.npmjs.com/package/cmd-dialog" target="_blank"><img src="https://img.shields.io/npm/v/cmd-dialog?style=for-the-badge" alt="Version"/></a>
+      <a href="https://github.com/OzzyCzech/cmd-dialog/blob/main/LICENSE" target="_blank"><img src="https://img.shields.io/npm/l/cmd-dialog?style=for-the-badge" alt="License"/></a>
+      <a href="https://github.com/OzzyCzech/cmd-dialog/commit/main" target="_blank"><img src="https://img.shields.io/github/last-commit/OzzyCzech/cmd-dialog?style=for-the-badge" alt="Last commit"/></a>
+      <a href="https://github.com/OzzyCzech/cmd-dialog/actions" target="_blank"><img src="https://img.shields.io/github/actions/workflow/status/OzzyCzech/cmd-dialog/main.yml?style=for-the-badge" alt="Workflow status"/></a>
+    </p>
+    <p className="mt-12 flex flex-wrap items-center justify-center gap-3">
+      <a href="https://cmd-dialog.vercel.app/" target="_blank" className="py-3 px-6 bg-gray-700 hover:bg-gray-600 rounded-full hover:text-current hover:no-underline inline-flex items-center"><ShareIcon/> Demo page</a>
+      <a href="https://github.com/OzzyCzech/cmd-dialog/" target="_blank" className="py-3 px-6 bg-gray-700 hover:bg-gray-600 rounded-full hover:text-current hover:no-underline inline-flex items-center"><GitHubIcon/> GitHub</a>
+    </p>
+  </article>
+
+
 // @see https://primer.style/css/utilities/colors
 const GitHub = () =>
   <article className="bg-[#f6f8fa] p-6 md:p-24 xl:p-32">
@@ -148,7 +177,7 @@ const GitHub = () =>
       <a href="https://github.com/OzzyCzech" target="_blank"
          className="py-3 px-6 bg-gray-900 hover:bg-black text-white rounded-full hover:text-white hover:no-underline inline-flex items-center"
          title="Visit my GitHub profile">
-        <ShareIcon/> Open GitHub
+        <GitHubIcon/> Open GitHub
       </a>
     </p>
   </article>;
@@ -163,6 +192,7 @@ const Work = () =>
       <Testomato/>
       <Zdrojak/>
       <Sphido/>
+      <CmdDialog/>
       <GitHub/>
     </main>
     <Footer/>
