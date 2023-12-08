a47245b02f4bab17a8b86268c0b58645e9d29c2b
Roman Ožana <roman@ozana.cz>
2023-12-08 09:40:53 +0100

    Add SpeedInsights

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,18 @@
-import Document, {Head, Html, Main, NextScript} from 'next/document'
+import {Head, Html, Main, NextScript} from 'next/document'
+import {SpeedInsights} from "@vercel/speed-insights/next";
 import React from 'react';
 
 export default () =>
-	<Html>
-		<Head>
-			<meta name="author" content="All: Roman Ožana; e-mail: roman@ozana.cz"/>
-			<link rel="shortcut icon" href="/favicon.svg"/>
-			<link rel="author" href="/humans.txt"/>
-		</Head>
+  <Html>
+    <Head>
+      <meta name="author" content="All: Roman Ožana; e-mail: roman@ozana.cz"/>
+      <link rel="shortcut icon" href="/favicon.svg"/>
+      <link rel="author" href="/humans.txt"/>
+    </Head>
 
-		<body className="font-sans text-gray-900 antialiased leading-normal tracking-normal">
-		<Main/>
-		<NextScript/>
-		</body>
-	</Html>
+    <body className="font-sans text-gray-900 antialiased leading-normal tracking-normal">
+    <Main/>
+    <NextScript/>
+    <SpeedInsights/>
+    </body>
+  </Html>
