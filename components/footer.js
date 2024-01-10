6fa1ab241b011bfa75e9a07a28895969ae32ce1d
Roman Ožana <roman@ozana.cz>
2024-01-10 13:02:04 +0100

    Remove Bluesky liml

--- components/footer.js
+++ components/footer.js
@@ -2,6 +2,5 @@ export default () => <footer className="print:hidden">
   <section className="border-t flex justify-center text-center py-6 lg:py-20">
     <a href="https://github.com/OzzyCzech/" target="_blank" className="no-underline text-current">GitHub</a><span className="mx-2">&bull;</span>
     <a href="https://stackoverflow.com/users/355316/ozzyczech/" target="_blank" className="no-underline text-current">StackOverflow</a><span className="mx-2">&bull;</span>
-    <a href="https://bsky.app/profile/ozana.cz" target="_blank" className="no-underline text-current">BlueSky</a>
   </section>
 </footer>
