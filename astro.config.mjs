0437c10465e0961bca5eac85b669ab0d6b1649b2
Roman Ožana <roman@ozana.cz>
2024-02-23 18:22:18 +0100

    Static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import {defineConfig} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
-
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({nesting: true})],
-  output: 'server',
   adapter: vercel(),
 });
