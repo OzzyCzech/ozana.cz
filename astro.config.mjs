9e782fae2d58f20d0773ecc74b5d1e1a77deb2d3
Roman Ožana <roman@ozana.cz>
2025-11-05 10:41:41 +0100

    Remove animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ozana.cz",
+	prefetch: true,
 	integrations: [sitemap(), tailwind()],
 	output: "server",
 	adapter: vercel({
